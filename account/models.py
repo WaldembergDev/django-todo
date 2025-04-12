79d8f2ecd51a668abbb18a66017eee8578dfa93e
waldembergdev <waldemberg.pereira@outlook.com>
2025-04-12 13:14:32 -0300

    Criado as classes User e CustomUserManager

---
+++ account/models.py
@@ -0,0 +1,34 @@
+from django.db import models
+from django.contrib.auth.models import BaseUserManager, AbstractBaseUser, PermissionsMixin
+from django.utils import timezone
+
+class CustomUserManager(BaseUserManager):
+    def create_user(self, name, email, password, **extra_fields):
+        email = self.normalize_email(email)
+        user = self.model(
+            name = name,
+            email = email,
+            **extra_fields
+        )
+        user.set_password(password)
+        user.save()
+        return user
+    
+    def create_superuser(self, name, email, password, **extra_fields):
+        extra_fields.setdefault('is_superuser', True)
+        extra_fields.setdefault('is_staff', True)
+        return self.create_user(name, email, password)
+
+
+# Create your models here.
+class User(AbstractBaseUser, PermissionsMixin):
+    name = models.CharField(max_length=120)
+    email = models.EmailField(unique=True)
+    is_staff = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=True)
+    date_joined = models.DateTimeField(default=timezone.now)
+
+    objects = CustomUserManager()
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['name']
